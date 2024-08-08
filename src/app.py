32b3992db3a22b197af6d649bbca6a6fda85cc8b
Viridiana De la cruz <167374369+viridlcrloya141999@users.noreply.github.com>
2024-08-08 01:56:37 +0000

    family static

--- src/app.py
+++ src/app.py
@@ -25,18 +25,36 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
 @app.route('/members', methods=['GET'])
 def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    return jsonify(members), 200
 
+@app.route('/member/<int:id>', methods=['GET'])
+def get_one_member(id):
+    one_member = jackson_family.get_member(id)
+    if one_member:
+        return jsonify(one_member), 200
+    else:
+        return jsonify({"message": "User not found"}), 404
 
-    return jsonify(response_body), 200
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    member_deleted = jackson_family.delete_member(id)
+    if member_deleted:
+        return jsonify({"done": True}), 200
+    else:
+        return jsonify({"message": "User not found"}), 404
+
+@app.route('/member', methods=['POST'])
+def add_new_member():
+    request_body = request.json
+    new_member = jackson_family.add_member(request_body)
+    if new_member:
+        return jsonify(new_member), 200
+    else:
+        return jsonify({'message': 'Bad request'}), 400
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
